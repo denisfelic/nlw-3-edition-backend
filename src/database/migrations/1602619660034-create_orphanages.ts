a7de80b64dd6e5a863a00d1992c45b722237aa08
Denis Feliciano <denisf.salles@gmail.com>
2020-10-14 01:35:23 -0300

    fixes, add await in create table

--- src/database/migrations/1602619660034-create_orphanages.ts
+++ src/database/migrations/1602619660034-create_orphanages.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class createOrphanages1602619660034 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.createTable(new Table({
+        await queryRunner.createTable(new Table({
             name: "orphanages",
             columns: [
                 {
