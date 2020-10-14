bf83c5a85394d37c09881cdcff9941bcc3701791
Denis Feliciano <denisf.salles@gmail.com>
2020-10-14 01:33:45 -0300

    import routes to external file, and use it in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 
 import express from 'express';
 import './database/connection';
+import routes from './routes';
+
 
 const app = express();
 app.use(express.json());
 
-
-app.post('/user/:id', (req, res) => {
-    
-    res.send({
-     });
-});
+app.use(routes);
 
 app.listen(3333);
 console.log('Server running server At 3333')
